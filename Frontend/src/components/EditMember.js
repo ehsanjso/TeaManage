d9c0f796fff22ca6976d5250352aa4b2d088536f
ehsan.jso <yek.jso@gmail.com>
2021-12-23 01:03:11 -0500

    Finalize EditMember design

--- Frontend/src/components/EditMember.js
+++ Frontend/src/components/EditMember.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Card } from "antd";
+import CardHeader from "./CardHeader";
+import MemberForm from "./MemberForm";
 
 export default function EditMember() {
-  return <div>EditMember</div>;
+  return (
+    <Card
+      title={
+        <CardHeader
+          header="Team members"
+          subHeader="You have 3 team members."
+          hasButton={false}
+        />
+      }
+      style={{ width: 400 }}
+    >
+      <MemberForm editMode={true} />
+    </Card>
+  );
 }
