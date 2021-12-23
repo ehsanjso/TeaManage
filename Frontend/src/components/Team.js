2f855ad3c370f1bc1683de71bda1825300790ae7
ehsan.jso <yek.jso@gmail.com>
2021-12-22 19:31:55 -0500

    Add add team member routing to the team card add button

--- Frontend/src/components/Team.js
+++ Frontend/src/components/Team.js
@@ -4,6 +4,7 @@ import { Button } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import MemberCard from "./MemberCard";
 import "../styles/components/team.scss";
+import { Link } from "react-router-dom";
 
 export default function Team() {
   return (
@@ -44,7 +45,9 @@ const CardHeader = (membersCount) => {
         <p>You have 3 team members.</p>
       </div>
       <div className="team-header__right">
-        <Button type="primary" shape="circle" icon={<PlusOutlined />} />
+        <Button type="primary" shape="round" icon={<PlusOutlined />}>
+          <Link to="/add-member">Add</Link>
+        </Button>
       </div>
     </div>
   );
