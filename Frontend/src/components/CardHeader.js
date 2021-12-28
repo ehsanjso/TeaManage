e1d1f84cc6171758c549088d57b36eef6263e04f
ehsan.jso <yek.jso@gmail.com>
2021-12-28 15:49:31 -0500

    Fix card header button click issue

--- Frontend/src/components/CardHeader.js
+++ Frontend/src/components/CardHeader.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../styles/components/card-header.scss";
 
 const CardHeader = ({ header, subHeader, hasButton }) => {
+  const navigate = useNavigate();
   return (
     <div className="team-header">
       <div className="team-header__left">
@@ -13,8 +14,15 @@ const CardHeader = ({ header, subHeader, hasButton }) => {
       </div>
       <div className="team-header__right">
         {hasButton && (
-          <Button type="primary" shape="round" icon={<PlusOutlined />}>
-            <Link to="/add-member">Add</Link>
+          <Button
+            type="primary"
+            shape="round"
+            icon={<PlusOutlined />}
+            onClick={() => {
+              navigate("/add-member");
+            }}
+          >
+            Add
           </Button>
         )}
       </div>
