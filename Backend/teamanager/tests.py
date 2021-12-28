8f179cbfc28eddf3c223b62e38d9a50dd477b4ff
ehsan.jso <yek.jso@gmail.com>
2021-12-28 14:37:36 -0500

    Add api test on django rest api

--- Backend/teamanager/tests.py
+++ Backend/teamanager/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TeamTestCase(APITestCase):
+    def add_member(self):
+        data = {"firstname": "test", "lastname": "case", "email": "test@localhost.com",
+                "phone": "2222222222", "isadmin": False}
+        response = self.client.post("/api/members/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
